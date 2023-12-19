5e4156405b760f47840062d359c4d90cb700ba2e
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2023-12-19 13:42:56 +0100

    More Hyperparameters

--- hyperparameter.py
+++ hyperparameter.py
@@ -1,23 +1,26 @@
+choices = ["bit", "eth", "ltc"]
+
 dataset_paths = {
-    'bit': "data/full_bitcoin.csv",
-    'eth': "data/full_ethereum.csv",
-    'ltc': "data/full_litecoin.csv"
+    "bit": "data/full_bitcoin.csv",
+    "eth": "data/full_ethereum.csv",
+    "ltc": "data/full_litecoin.csv"
 }
 
 batch_sizes = {
-    'bit': 32,
-    'eth': 32,
-    'ltc': 32
+    "bit": 32,
+    "eth": 32,
+    "ltc": 32
 }
 
 epochs = {
-    'bit': 20,
-    'eth': 200,
-    'ltc': 20
+    "bit": 20,
+    "eth": 200,
+    "ltc": 20
 }
 
 durations = {
-    'bit': 50,
-    'eth': 50,
-    'ltc': 50
+    "bit": 50,
+    "eth": 50,
+    "ltc": 50
+
 }
