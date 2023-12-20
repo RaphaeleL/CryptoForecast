f41f2b105282d2589bb4c3ace57451aff2f137fa
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2023-12-20 22:28:21 +0100

    Different Model

--- forecast.py
+++ forecast.py
@@ -4,9 +4,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
-from hyperparameter import batch_sizes, epochs, dataset_paths, durations, choices, dataset_names
+from hyperparameter import dataset_paths, choices, dataset_names
 from keras import Sequential
-from keras.layers import Dense, LSTM
+from keras.layers import Dense, LSTM, Conv1D, Flatten, Bidirectional, Dropout, GRU, TimeDistributed, MaxPool1D, BatchNormalization
+from keras.regularizers import l1, l2, l1_l2
+from keras.optimizers.legacy import SGD, Adam
 
 def load_and_preprocess_data(path):
     """ Load and preprocess data from the given path. """
@@ -17,6 +19,9 @@ def load_and_preprocess_data(path):
         elif "coinmarketcap" in path:
             to_drop = ["timeOpen", "timeClose", "timeHigh", "timeLow", "name", "open", "high", "low", "volume", "marketCap", "timestamp"]
             data = pd.read_csv(path, sep=";")
+        elif "yahoo" in path:
+            to_drop = ["Date", "Open", "High", "Low", "Adj Close", "Volume"]
+            data = pd.read_csv(path)
         for column in to_drop:
             if column in data.columns:
                 data = data.drop(column, axis=1)
@@ -25,89 +30,86 @@ def load_and_preprocess_data(path):
         return data
     except FileNotFoundError:
         print(f"File not found: {path}")
-        exit(1)
 
 def normalize_data(data):
     """ Normalize data using Min-Max Scaler. """
     scaler = MinMaxScaler(feature_range=(0, 1))
     return scaler, scaler.fit_transform(data)
 
-def create_dataset(dataset, duration):
-    """ Create dataset matrix for training and testing. """
+def create_dataset(dataset):
+    """ Create dataset matrix for training and testing using the full dataset. """
     dataX, dataY = [], []
-    for i in range(len(dataset) - duration):
-        a = dataset[i:(i + duration), 0]
+    for i in range(len(dataset) - 1):
+        a = dataset[i:i + 1, 0]
         dataX.append(a)
-        dataY.append(dataset[i + duration, 0])
-    return np.array(dataX).reshape(-1, duration, 1), np.array(dataY)
+        dataY.append(dataset[i + 1, 0])
+    return np.array(dataX).reshape(-1, 1, 1), np.array(dataY)
 
-def build_and_compile_model(duration, num_features):
+def build_and_compile_model(num_features):
     """ Build and compile the Keras Sequential model. """
+
     model = Sequential([
-        LSTM(250, activation="relu", input_shape=(duration, num_features), return_sequences=True),
-        LSTM(250, activation="relu", return_sequences=True),
-        LSTM(250, activation="relu", return_sequences=True),
-        LSTM(250, activation="relu", return_sequences=False),
-        Dense(25, activation="relu"),
+        Conv1D(64, 1, activation="relu", input_shape=(1, num_features)),
+        BatchNormalization(),
+        Bidirectional(LSTM(50, activation="relu", return_sequences=True)),
+        BatchNormalization(),
+        Bidirectional(LSTM(50, activation="relu", return_sequences=True)),
+        BatchNormalization(),
+        Dropout(0.2),
+        Flatten(),
+        Dense(50, activation='relu', kernel_regularizer=l2(0.001)),  # L2 regularization
+        BatchNormalization(),
         Dense(1)
     ])
-    model.compile(optimizer="adam", loss="mean_squared_error")
+    model.compile(optimizer=Adam(0.001), loss="mse", metrics=["mae"])
     return model
 
-def plot_predictions(testPredict, coin, dataset_name, testY=None, ):
+def plot_predictions(testPredict, coin, reverse_plot, dataset_name, testY=None, ):
     """ Plot the actual vs predicted prices. """
-    if testY is not None: plt.plot(testY, label="Actual Price")
+    if testY is not None: plt.plot(testY[::-1], label="Actual Price")
     title = f"{coin} Price Prediction \n{dataset_name}"
-    plt.plot(testPredict[::-1], label="Predicted Price")
+    plt.plot(testPredict if not reverse_plot else testPredict[::-1], label="Predicted Price")
     plt.xlabel("Days")
     plt.ylabel("Price in $")
     plt.title(title)
     plt.grid(True)
     plt.legend()
+    plt.tight_layout()
     plt.show()
 
-def print_predictions(testPredict, coin, dataset_name):
-    """ Print the actual vs predicted prices. """
-    print(f"Predictions for {coin} using {dataset_name}:")
-    for key, value in enumerate(testPredict):
-        print(f"Day {key}: ${value[0]:.2f}")
-
-def main(coin, batch_size, epochs, duration, dataset_path, dataset_name, real_pred=False, print_pred=False):
+def main(coin, batch_size, epochs, dataset_path, dataset_name, real_pred, reverse_plot):
     data = load_and_preprocess_data(dataset_path)
     scaler, normalized_data = normalize_data(data)
-    X, y = create_dataset(normalized_data, duration)
-    if not real_pred:
+    X, y = create_dataset(normalized_data)
+    if real_pred == -1:
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, shuffle=False)
-        model = build_and_compile_model(duration, X_train.shape[2])
+        model = build_and_compile_model(X_train.shape[2])
         model.fit(X_train, y_train, batch_size=batch_size, epochs=epochs, verbose=1)
         testPredict = scaler.inverse_transform(model.predict(X_test))
         testY = scaler.inverse_transform(y_test.reshape(-1, 1))
-        plot_predictions(testPredict[-duration + 1:], coin, dataset_name, testY=testY[-duration:])
+        plot_predictions(testPredict, coin, reverse_plot, dataset_name, testY=testY)
     else: 
-        model = build_and_compile_model(duration, X.shape[2])
+        model = build_and_compile_model(X.shape[2])
         model.fit(X, y, batch_size=batch_size, epochs=epochs, verbose=1)
         testPredict = scaler.inverse_transform(model.predict(X))
-        if print_pred: print_predictions(testPredict[-duration + 1:], coin, dataset_name)
-        plot_predictions(testPredict[-duration + 1:], coin, dataset_name)
+        plot_predictions(testPredict[-real_pred:], coin, reverse_plot, dataset_name)
 
 if __name__ == "__main__":
     argparser = argparse.ArgumentParser(description="Cryptocurrency Price Prediction")
-    argparser.add_argument("--coin", type=str, choices=choices, required=True, help=f"Specify the cryptocurrency: {choices}")
-    argparser.add_argument("--dataset_name", type=str, choices=dataset_names, required=True, help=f"Specify the dataset: {choices}")
-    argparser.add_argument("--batch_size", type=int, help="Batch size for training")
-    argparser.add_argument("--epochs", type=int, help="Number of epochs for training")
-    argparser.add_argument("--duration", type=int, help="Forecast Horizon")
-    argparser.add_argument("--real_prediction", action="store_true", help="Real prediction")
-    argparser.add_argument("--print_result", action="store_true", help="Print the Result of the Prediction")
+    argparser.add_argument("--coin", type=str, default="eth", choices=choices)
+    argparser.add_argument("--dataset", type=str, default="yahoo", choices=dataset_names)
+    argparser.add_argument("--batch_size", type=int, default=32)
+    argparser.add_argument("--epochs", type=int, default=100)
+    argparser.add_argument("--prediction", type=int, default=-1)
+    argparser.add_argument("--revplot", action="store_true", default=-1)
     args = argparser.parse_args()
 
     main(
         coin=args.coin.upper(), 
-        batch_size=batch_sizes[args.coin] if not args.batch_size else args.batch_size, 
-        epochs=epochs[args.coin] if not args.epochs else args.epochs, 
-        duration=durations[args.coin] if not args.duration else args.duration,
-        dataset_path=dataset_paths[args.coin][args.dataset_name],
-        dataset_name=args.dataset_name,
-        real_pred=args.real_prediction,
-        print_pred=args.print_result
+        batch_size=args.batch_size, 
+        epochs=args.epochs, 
+        dataset_path=dataset_paths[args.coin][args.dataset],
+        dataset_name=args.dataset,
+        real_pred=args.prediction,
+        reverse_plot=args.revplot
     )
