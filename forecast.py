ffc9d0a71e2bc43b7546bb65d02d4cead3f1a983
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2023-12-21 08:44:14 +0100

    KFold Cross Valid.

--- forecast.py
+++ forecast.py
@@ -4,11 +4,14 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
-from hyperparameter import dataset_paths, choices, dataset_names
+from sklearn.model_selection import KFold
+from sklearn.metrics import mean_squared_error
 from keras import Sequential
 from keras.layers import Dense, LSTM, Conv1D, Flatten, Bidirectional, Dropout, GRU, TimeDistributed, MaxPool1D, BatchNormalization
 from keras.regularizers import l1, l2, l1_l2
 from keras.optimizers.legacy import SGD, Adam
+
+from hyperparameter import dataset_paths, choices, dataset_names
 
 def load_and_preprocess_data(path):
     """ Load and preprocess data from the given path. """
@@ -21,6 +24,9 @@ def load_and_preprocess_data(path):
             data = pd.read_csv(path, sep=";")
         elif "yahoo" in path:
             to_drop = ["Date", "Open", "High", "Low", "Adj Close", "Volume"]
+            data = pd.read_csv(path)
+        elif "gecko" in path:
+            to_drop = ["snapped_at", "market_cap", "total_volume"]
             data = pd.read_csv(path)
         for column in to_drop:
             if column in data.columns:
@@ -50,21 +56,17 @@ def build_and_compile_model(num_features):
 
     model = Sequential([
         Conv1D(64, 1, activation="relu", input_shape=(1, num_features)),
-        BatchNormalization(),
         Bidirectional(LSTM(50, activation="relu", return_sequences=True)),
-        BatchNormalization(),
         Bidirectional(LSTM(50, activation="relu", return_sequences=True)),
-        BatchNormalization(),
         Dropout(0.2),
         Flatten(),
-        Dense(50, activation='relu', kernel_regularizer=l2(0.001)),  # L2 regularization
-        BatchNormalization(),
+        Dense(50, activation='relu', kernel_regularizer=l2(0.001)),
         Dense(1)
     ])
     model.compile(optimizer=Adam(0.001), loss="mse", metrics=["mae"])
     return model
 
-def plot_predictions(testPredict, coin, reverse_plot, dataset_name, testY=None, ):
+def plot_predictions(testPredict, coin="n/a", reverse_plot=False, dataset_name="n/a", testY=None):
     """ Plot the actual vs predicted prices. """
     if testY is not None: plt.plot(testY[::-1], label="Actual Price")
     title = f"{coin} Price Prediction \n{dataset_name}"
@@ -81,18 +83,30 @@ def main(coin, batch_size, epochs, dataset_path, dataset_name, real_pred, revers
     data = load_and_preprocess_data(dataset_path)
     scaler, normalized_data = normalize_data(data)
     X, y = create_dataset(normalized_data)
+
+    kf = KFold(n_splits=5, shuffle=False)
+    all_test_predictions = []
+    all_test_actuals = []
+
     if real_pred == -1:
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, shuffle=False)
-        model = build_and_compile_model(X_train.shape[2])
-        model.fit(X_train, y_train, batch_size=batch_size, epochs=epochs, verbose=1)
-        testPredict = scaler.inverse_transform(model.predict(X_test))
-        testY = scaler.inverse_transform(y_test.reshape(-1, 1))
-        plot_predictions(testPredict, coin, reverse_plot, dataset_name, testY=testY)
-    else: 
-        model = build_and_compile_model(X.shape[2])
-        model.fit(X, y, batch_size=batch_size, epochs=epochs, verbose=1)
-        testPredict = scaler.inverse_transform(model.predict(X))
-        plot_predictions(testPredict[-real_pred:], coin, reverse_plot, dataset_name)
+        for train_index, test_index in kf.split(X):
+            X_train, X_test = X[train_index], X[test_index]
+            y_train, y_test = y[train_index], y[test_index]
+            model = build_and_compile_model(X.shape[2])
+            model.fit(X_train, y_train, batch_size=batch_size, epochs=epochs, verbose=1)
+            testPredict = scaler.inverse_transform(model.predict(X_test))
+            testY = scaler.inverse_transform(y_test.reshape(-1, 1))
+            all_test_predictions.extend(testPredict)
+            all_test_actuals.extend(testY)
+        plot_predictions(np.concatenate(all_test_predictions), coin, reverse_plot, dataset_name, testY=all_test_actuals)
+    else:
+        for train_index, _ in kf.split(X):
+            X_train, y_train = X[train_index], y[train_index]
+            model = build_and_compile_model(X.shape[2])
+            model.fit(X_train, y_train, batch_size=batch_size, epochs=epochs, verbose=1)
+            testPredict = scaler.inverse_transform(model.predict(X))
+            all_test_predictions.extend(testPredict)
+        plot_predictions(np.concatenate(all_test_predictions)[-real_pred:], coin, reverse_plot, dataset_name)
 
 if __name__ == "__main__":
     argparser = argparse.ArgumentParser(description="Cryptocurrency Price Prediction")
