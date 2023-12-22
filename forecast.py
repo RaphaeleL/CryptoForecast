a75286b97a2b9defa8a51ad4968931dcb55fcf32
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2023-12-22 13:59:44 +0100

    More CLI, Agents, etc.

--- forecast.py
+++ forecast.py
@@ -1,13 +1,12 @@
-import yfinance as yf
 import argparse
-import numpy as np
 from sklearn.model_selection import KFold
 from keras import Sequential
 from keras.layers import Dense, LSTM, Conv1D, Flatten, Bidirectional, Dropout
-from keras.regularizers import l2, l1_l2
+from keras.regularizers import l2
 from keras.optimizers.legacy import Adam
+from tqdm.keras import TqdmCallback
 
-from utils import load_and_preprocess_data, normalize_data, create_dataset, plot_predictions
+from utils import load_and_preprocess_data, normalize_data, create_dataset, plot, plot_all_agents
 
 def build_and_compile_model(num_features):
     """ Build and compile the Keras Sequential model. """
@@ -20,38 +19,44 @@ def build_and_compile_model(num_features):
         Dense(50, activation='relu', kernel_regularizer=l2(0.001)),
         Dense(1)
     ])
-    model.compile(optimizer=Adam(0.001), loss="mse", metrics=["mae"])
+    model.compile(optimizer=Adam(0.001), loss="mse")
     return model
 
-def main(coin, batch_size, epochs, prediction):
+def main(coin, batch_size, epochs, prediction, agents, folds, plot_coin, reverse):
+    all_agent_test_predictions = []
+    all_agent_test_actuals = []
+    for agent in range(agents):
+        data = load_and_preprocess_data(coin, reverse)
+        if plot_coin: plot(coin, data, exit_after=True)
+        scaler, normalized_data = normalize_data(data)
+        X, y = create_dataset(normalized_data)
 
-    data = load_and_preprocess_data(coin)
-    scaler, normalized_data = normalize_data(data)
-    X, y = create_dataset(normalized_data)
+        kf = KFold(n_splits=folds, shuffle=False)
+        all_test_predictions = []
+        all_test_actuals = []
 
-    kf = KFold(n_splits=5, shuffle=False)
-    all_test_predictions = []
-    all_test_actuals = []
-
-    if prediction == -1:
-        for train_index, test_index in kf.split(X):
-            X_train, X_test = X[train_index], X[test_index]
-            y_train, y_test = y[train_index], y[test_index]
-            model = build_and_compile_model(X.shape[2])
-            model.fit(X_train, y_train, batch_size=batch_size, epochs=epochs, verbose=1)
-            testPredict = scaler.inverse_transform(model.predict(X_test))
-            testY = scaler.inverse_transform(y_test.reshape(-1, 1))
-            all_test_predictions.extend(testPredict)
-            all_test_actuals.extend(testY)
-        plot_predictions(np.concatenate(all_test_predictions), coin, testY=all_test_actuals)
-    else:
-        for train_index, _ in kf.split(X):
-            X_train, y_train = X[train_index], y[train_index]
-            model = build_and_compile_model(X.shape[2])
-            model.fit(X_train, y_train, batch_size=batch_size, epochs=epochs, verbose=1)
-            testPredict = scaler.inverse_transform(model.predict(X))
-            all_test_predictions.extend(testPredict)
-        plot_predictions(np.concatenate(all_test_predictions)[-prediction:], coin)
+        if prediction == -1:
+            for index, (train_index, test_index) in enumerate(kf.split(X)):
+                print(f"Training Agent {agent+1}/{agents} for Fold {index+1}/{folds}")
+                X_train, X_test = X[train_index], X[test_index]
+                y_train, y_test = y[train_index], y[test_index]
+                model = build_and_compile_model(X.shape[2])
+                model.fit(X_train, y_train, batch_size=batch_size, epochs=epochs, verbose=0, callbacks=[TqdmCallback(verbose=2)])
+                testPredict = scaler.inverse_transform(model.predict(X_test))
+                testY = scaler.inverse_transform(y_test.reshape(-1, 1))
+                all_test_predictions.extend(testPredict)
+                all_test_actuals.extend(testY)
+        else:
+            for index, (train_index, _) in enumerate(kf.split(X)):
+                print(f"Training Agent {agent+1}/{agents} for Fold {index+1}/{folds}")
+                X_train, y_train = X[train_index], y[train_index]
+                model = build_and_compile_model(X.shape[2])
+                model.fit(X_train, y_train, batch_size=batch_size, epochs=epochs, verbose=0, callbacks=[TqdmCallback(verbose=2)])
+                testPredict = scaler.inverse_transform(model.predict(X[-prediction:]))
+                all_test_predictions.extend(testPredict[-prediction:])
+        all_agent_test_predictions.append(all_test_predictions)
+        all_agent_test_actuals.append(all_test_actuals)
+    plot_all_agents(all_agent_test_predictions, coin, all_agent_test_actuals if prediction == -1 else None)
 
 if __name__ == "__main__":
     argparser = argparse.ArgumentParser(description="Cryptocurrency Price Prediction")
@@ -59,11 +64,19 @@ if __name__ == "__main__":
     argparser.add_argument("--batch_size", type=int, default=32)
     argparser.add_argument("--epochs", type=int, default=100)
     argparser.add_argument("--prediction", type=int, default=-1)
+    argparser.add_argument("--agents", type=int, default=1)
+    argparser.add_argument("--folds", type=int, default=5)
+    argparser.add_argument("--plot_coin", action="store_true")
+    argparser.add_argument("--reverse", action="store_true")
     args = argparser.parse_args()
 
     main(
-        coin=f"{args.coin.upper()}-USD", 
+        coin=f"{args.coin.upper()}", 
         batch_size=args.batch_size, 
         epochs=args.epochs, 
         prediction=args.prediction,
+        agents=args.agents,
+        folds=args.folds,
+        plot_coin=args.plot_coin,
+        reverse=args.reverse
     )
--- utils.py
+++ utils.py
@@ -1,10 +1,19 @@
+import math
 import yfinance as yf
 import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
 from sklearn.preprocessing import MinMaxScaler
 
-def load_and_preprocess_data(coin):
+def plot(coin, data, exit_after=True):
+    """ Plot the given data. """
+    plt.title(f"{coin} Price History")
+    plt.plot(data)
+    plt.grid(True)
+    plt.show()
+    if exit_after: exit()
+
+def load_and_preprocess_data(coin, reverse=False):
     """ Load and preprocess data from the given path. """
     data = pd.DataFrame(yf.Ticker(coin).history(period="max"))
     data_columns = data.columns.to_list()
@@ -13,7 +22,7 @@ def load_and_preprocess_data(coin):
         if column in data.columns:
             data = data.drop(column, axis=1)
     data = data.replace("\.", "", regex=True).replace(",", ".", regex=True).astype(float)
-    data = data.iloc[::-1].reset_index(drop=True)
+    if reverse: data = data.iloc[::-1].reset_index(drop=True)
     return data
 
 def normalize_data(data):
@@ -30,7 +39,7 @@ def create_dataset(dataset):
         dataY.append(dataset[i + 1, 0])
     return np.array(dataX).reshape(-1, 1, 1), np.array(dataY)
 
-def plot_predictions(testPredict, coin="n/a", testY=None):
+def plot_predictions(testPredict, coin, testY=None):
     """ Plot the actual vs predicted prices. """
     if testY is not None: plt.plot(testY[::-1], label="Actual Price")
     title = f"{coin} Price Prediction"
@@ -40,5 +49,29 @@ def plot_predictions(testPredict, coin="n/a", testY=None):
     plt.title(title)
     plt.grid(True)
     plt.legend()
+    plt.tight_layout()
+    plt.show()
+
+def plot_all_agents(all_agent_predictions, coin, all_agent_actuals=None):
+    """ Plot predictions and actuals for all agents in a single GUI window. """
+    num_agents = len(all_agent_predictions)
+    rows = math.ceil(math.sqrt(num_agents))
+    cols = math.ceil(num_agents / rows)
+    plt.figure(figsize=(15, rows * 5))
+
+    for i in range(num_agents):
+        predictions = all_agent_predictions[i]
+        plt.subplot(rows, cols, i + 1)
+        plt.plot(predictions, label=f"Agent {i+1} Predictions")
+        if all_agent_actuals: 
+            actuals = all_agent_actuals[i]
+            plt.plot(actuals, label=f"Agent {i+1} Actuals", alpha=0.7)
+        plt.title(f"{coin} Price Prediction by Agent {i+1}")
+        plt.xlabel("Days")
+        plt.ylabel("Price in $")
+        # plt.yticks([])
+        plt.legend()
+        plt.grid(True)
+
     plt.tight_layout()
     plt.show()
