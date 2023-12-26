53c534eb4a6baa9505fb67c2a3c5502ce1f9f412
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2023-12-26 18:26:15 +0100

    Major Updates
    - Date Index
    - Use best Agent
    - 2 Subplots
      - 1st: History
      - 2nd: Future

--- forecast.py
+++ forecast.py
@@ -1,21 +1,34 @@
 import threading
 import pandas as pd
 from sklearn.model_selection import KFold
+from matplotlib import pyplot as plt
 
 from utils import (
+    evaluate_agent_performance,
+    select_best_agent,
+    need_retraining,
     load_and_preprocess_data,
     normalize_data,
     create_dataset,
-    plot,
-    plot_all_agents,
+    plot, 
+    plot_all,
     split, 
     train,
     argument_parser,
-    check
+    check,
+    print_colored,
+    print_data
 )
 
-def crypto_forecast(coin, batch_size, epochs, agent, agents, folds, reverse, test_pred, test_actu, real_pred):
-    data = load_and_preprocess_data(coin, reverse)
+def map_predictions_to_dates(start_date, predictions):
+    """Map predictions to dates."""
+    dates = pd.date_range(start_date, periods=len(predictions), freq='H')
+    date_predictions = pd.DataFrame(predictions, index=dates, columns=['Prediction'])
+    return date_predictions
+
+def crypto_forecast(coin, batch_size, epochs, agent, agents, folds, test_pred, test_actu, real_pred, prediction_days=1):
+    """Forecast cryptocurrency prices."""
+    data = load_and_preprocess_data(coin)
     scaler, normalized_data = normalize_data(data)
     X, y = create_dataset(normalized_data)
     kf = KFold(n_splits=folds, shuffle=False)
@@ -25,53 +38,51 @@ def crypto_forecast(coin, batch_size, epochs, agent, agents, folds, reverse, tes
     real_predictions = []
 
     for index, (train_index, test_index) in enumerate(kf.split(X)):
-        print(f"Training Agent {agent+1}/{agents} for Fold {index+1}/{folds}")
+        print_colored(f"Training Agent {agent+1}/{agents} for Fold {index+1}/{folds}", "green")
         X_train, X_test, y_train, y_test = split(X, y, train_index, test_index)
         model = train(X, y, X_train, y_train, batch_size, epochs)
         prediction = scaler.inverse_transform(model.predict(X_test))
         predictions.extend(prediction)
         actuals.extend(scaler.inverse_transform(y_test.reshape(-1, 1)))
 
-    test_pred.append(predictions)
-    test_actu.append(actuals)
+    test_dates = data.index[test_index].to_pydatetime()
+    test_pred.append(pd.DataFrame(prediction, index=test_dates, columns=['Prediction']))
+    test_actu.append(pd.DataFrame(scaler.inverse_transform(y_test.reshape(-1, 1)), index=test_dates, columns=['Actual']))
 
     for index, (train_index, _) in enumerate(kf.split(X)):
-        print(f"Training Agent {agent+1}/{agents} for Fold {index+1}/{folds}")
+        print_colored(f"Training Agent {agent+1}/{agents} for Fold {index+1}/{folds}", "green")
         X_train, y_train = split(X, y, train_index) 
         model = train(X, y, X_train, y_train, batch_size, epochs)
-        prediction = scaler.inverse_transform(model.predict(X[-10:]))
+        prediction = scaler.inverse_transform(model.predict(X[-(prediction_days*12):]))
         real_predictions.extend(prediction)
 
     starting_index = len(test_pred[0]) + 1
-    real_pred_indices = list(range(starting_index, starting_index + len(real_predictions)))
+    last_day = test_actu[-1].index[-1]
+    next_day = last_day + pd.Timedelta(hours=1)
+    future_dates = pd.date_range(start=next_day, periods=len(real_predictions), freq='H')
+    real_pred.append(pd.DataFrame(real_predictions, index=future_dates, columns=['Prediction']))
 
-    real_pred.append(pd.Series(real_predictions, index=real_pred_indices))
-
-    if not check(test_actu[-1][-1], real_pred[0][starting_index]):
-        print("*****************************************************")
-        print(f"***** ERROR (A{agent+1}): Prediction is not accurate enough.")
-        print(f"***** Actual Value: {test_actu[-1][-1]}")
-        print(f"***** Predicted:: {real_pred[0][starting_index]}")
-        print("*****************************************************")
-        # TODO - Add a way to retrain the model
+    if not check(test_actu[-1].iloc[-1].item(), real_pred[0].iloc[starting_index].item()):
+        border = "*****************************************************"
+        print_colored(border, "red")
+        print_colored(f"ERROR (Agent {agent+1})", "red")
+        print_colored("Prediction is not accurate enough.", "red")
+        print_colored(f"  > Actual Value:    {round(test_actu[-1][-1][0])}", "green")
+        print_colored(f"  > Predicted Value: {round(real_pred[0][starting_index][0])}", "blue")
+        print_colored(border, "red")
 
     return test_pred, test_actu, real_pred
 
-if __name__ == "__main__":
+def main():
+    # TODO: print a list of agents and folds with done, in progress and not started
     args = argument_parser()
-
-    if args.plot_coin:
-        data = load_and_preprocess_data(args.coin, args.reverse)
-        plot(args.coin, data, exit_after=True)
-        exit()
-
     threads, test_pred, test_actu, real_pred = [], [], [], []
 
     for agent in range(args.agents):
         thread = threading.Thread(
             target=crypto_forecast,
-            args=(args.coin, args.batch_size, args.epochs, agent, args.agents, args.folds,
-                args.reverse, test_pred, test_actu, real_pred),
+            args=(args.coin, args.batch_size, args.epochs, agent, args.agents, 
+                  args.folds, test_pred, test_actu, real_pred, args.prediction),
         )
         threads.append(thread)
         thread.start()
@@ -79,4 +90,17 @@ if __name__ == "__main__":
     for thread in threads:
         thread.join()
 
-    plot_all_agents(args.coin, test_pred, test_actu, real_pred)
+    performance_data = evaluate_agent_performance(test_actu, test_pred)
+    best_agent = select_best_agent(performance_data)
+    
+    if need_retraining(best_agent, performance_data):
+        # TODO: We need to retrain the model.
+        print_colored("Need to Train the Model again...", "red")
+
+    print_data(real_pred)
+    if args.show_all:
+        plot_all(args.coin, best_agent, real_pred)
+    plot(args.coin, best_agent, test_pred[best_agent], test_actu[best_agent], real_pred[best_agent])
+
+if __name__ == "__main__":
+    main()
--- utils.py
+++ utils.py
@@ -4,8 +4,9 @@ import yfinance as yf
 import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
-from sklearn.model_selection import KFold
+from matplotlib import dates as mdates
 from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_absolute_error
 
 from keras import Sequential
 from keras.layers import Dense, LSTM, Conv1D, Flatten, Bidirectional, Dropout
@@ -13,37 +14,25 @@ from keras.regularizers import l2
 from keras.optimizers.legacy import Adam
 from tqdm.keras import TqdmCallback
 
-def plot(coin, data, exit_after=True):
-    """Plot the given data."""
-    plt.title(f"{coin} Price History")
-    plt.plot(data)
-    plt.grid(True)
-    plt.show()
-    if exit_after:
-        exit()
-
-
-def load_and_preprocess_data(coin, reverse=False):
+def load_and_preprocess_data(ticker):
     """Load and preprocess data from the given path."""
-    data = pd.DataFrame(yf.Ticker(coin).history(period="max"))
-    data_columns = data.columns.to_list()
-    data_columns.remove("Close")
-    for column in data_columns:
-        if column in data.columns:
-            data = data.drop(column, axis=1)
-    data = (
-        data.replace("\.", "", regex=True).replace(",", ".", regex=True).astype(float)
-    )
-    if reverse:
-        data = data.iloc[::-1].reset_index(drop=True)
-    return data
+    data = yf.download(ticker)
+    data = data[['Close']]
+    data.reset_index(inplace=True)
+    data.set_index('Date', inplace=True)
+    return stretch_data(data)
 
+def stretch_data(data, stretch_factor=24):
+    """Stretch daily data to a specified hourly interval."""
+    data.index = pd.to_datetime(data.index)
+    interval = f'{int(24 / stretch_factor)}H'
+    stretched = data.resample(interval).interpolate(method='time')
+    return stretched
 
 def normalize_data(data):
     """Normalize data using Min-Max Scaler."""
     scaler = MinMaxScaler(feature_range=(0, 1))
     return scaler, scaler.fit_transform(data)
-
 
 def create_dataset(dataset):
     """Create dataset matrix for training and testing using the full dataset."""
@@ -54,42 +43,61 @@ def create_dataset(dataset):
         dataY.append(dataset[i + 1, 0])
     return np.array(dataX).reshape(-1, 1, 1), np.array(dataY)
 
-
-def plot_predictions(testPredict, coin, testY=None):
-    """Plot the actual vs predicted prices."""
-    if testY is not None:
-        plt.plot(testY[::-1], label="Actual Price")
-    title = f"{coin} Price Prediction"
-    plt.plot(testPredict[::-1], label="Predicted Price")
-    plt.xlabel("Days")
-    plt.ylabel("Price in $")
-    plt.title(title)
-    plt.grid(True)
-    plt.legend()
-    plt.tight_layout()
-    plt.show()
-
-
-def plot_all_agents(coin, train, test, val):
+def plot_all(coin, best_agent, val):
     """Plot predictions and actuals for all agents in a single GUI window."""
-    num_agents = len(train)
+    num_agents = len(val)
     rows = math.ceil(math.sqrt(num_agents))
     cols = math.ceil(num_agents / rows)
     plt.figure(figsize=(15, rows * 5))
+    
     for i in range(num_agents):
-        plt.subplot(rows, cols, i + 1)
-        plt.plot(train[i], label=f"Agent {i+1} Predictions")
-        plt.plot(test[i], label=f"Agent {i+1} Actuals", alpha=0.7)
-        plt.plot(val[i], label=f"Agent {i+1} Real Predictions")
-        plt.title(f"{coin} Price Prediction by Agent {i+1}")
-        plt.xlabel("Days")
-        plt.ylabel("Price in $")
-        plt.legend()
-        plt.grid(True)
+        if i == best_agent: title = f"{coin} Price Prediction by BEST Agent {i+1}"
+        else: title = f"{coin} Price Prediction by Agent {i+1}"
+        ax = plt.subplot(rows, cols, i + 1)
+        ax.plot(val[i].index, val[i]['Prediction'], label=f"Agent {i+1} Real Predictions")
+        ax.set_title(title)
+        ax.set_xlabel("Days")
+        ax.set_ylabel("Price in $")
+        ax.legend()
+        ax.grid(True)
+        ax.xaxis_date()
+        ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
+        ax.xaxis.set_major_locator(mdates.DayLocator(interval=30)) 
+        plt.setp(ax.get_xticklabels(), rotation=45, ha='right')
+    # plt.savefig(f"images/{coin}_{pd.to_datetime("today").strftime("%Y-%m-%d")}.png")
+    plt.tight_layout()
+    plt.show()
+
+def plot(coin, agent, train, test, val):
+    """Plot predictions and actuals for the best agents in a single GUI window."""
+    length = 2
+    _, axs = plt.subplots(length, 1, figsize=(15, 10))
+    
+    axs[0].plot(train.index, train['Prediction'], label=f"Train Predictions")
+    axs[0].plot(test.index, test['Actual'], label=f"Test Actuals", alpha=0.7)
+    axs[0].set_title(f"{coin} Train/Test by Agent {agent+1}")
+    axs[0].set_xlabel("Days")
+    axs[0].set_ylabel("Price in $")
+    axs[0].xaxis.set_major_locator(mdates.DayLocator(interval=90))
+
+    axs[1].plot(val.index, val['Prediction'], label=f"Future Predictions")
+    axs[1].set_title(f"{coin} Future Predictions by Agent {agent+1}")
+    axs[1].set_xlabel("Days")
+    axs[1].set_ylabel("Price in $")
+    axs[1].xaxis.set_major_locator(mdates.DayLocator(interval=1))
+
+    for i in range(length):
+        axs[i].legend()
+        axs[i].grid(True)
+        axs[i].xaxis_date()
+        axs[i].xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
+        plt.setp(axs[i].get_xticklabels(), rotation=45, ha='right')
+
     plt.tight_layout()
     plt.show()
 
 def split(X, y, train_index, test_index=None):
+    """Split the dataset into training and testing sets."""
     X_train = X[train_index]
     y_train = y[train_index]
     if test_index is not None:
@@ -115,6 +123,7 @@ def build_and_compile_model(num_features):
     return model
 
 def train(X, y, X_train, y_train, batch_size, epochs):
+    """Train the model."""
     model = build_and_compile_model(X.shape[2])
     model.fit(
         X_train,
@@ -127,18 +136,59 @@ def train(X, y, X_train, y_train, batch_size, epochs):
     return model
 
 def argument_parser():
+    """Parse command line arguments."""
     argparser = argparse.ArgumentParser(description="Cryptocurrency Price Prediction")
-    argparser.add_argument("--coin", type=str, default="eth")
+    argparser.add_argument("--coin", type=str, default="ETH-EUR")
     argparser.add_argument("--batch_size", type=int, default=32)
-    argparser.add_argument("--epochs", type=int, default=100)
-    argparser.add_argument("--agents", type=int, default=1)
+    argparser.add_argument("--epochs", type=int, default=20)
+    argparser.add_argument("--agents", type=int, default=4)
     argparser.add_argument("--folds", type=int, default=5)
-    argparser.add_argument("--plot_coin", action="store_true")
-    argparser.add_argument("--reverse", action="store_true")
+    argparser.add_argument("--prediction", type=int, default=7)
+    argparser.add_argument("--show_all", action='store_true')
     args = argparser.parse_args()
     return args
 
-def check(first_pred, last_actual, p=0.10):
+def check(first_pred, last_actual, p=0.05):
+    """Check if the prediction is within the given percentage of the actual value."""
     if first_pred >= last_actual * (1-p) and first_pred <= last_actual * (1+p):
         return True
     return False
+
+def print_colored(to_print, color, end="\n"):
+    """Print text in the given color."""
+    colors = {
+        "red": "\033[91m",
+        "green": "\033[92m",
+        "yellow": "\033[93m",
+        "blue": "\033[94m",
+        "purple": "\033[95m",
+        "cyan": "\033[96m",
+        "end": "\033[0m",
+    }
+    print(f"{colors.get(color, '')}{to_print}{colors['end']}", end=end)
+
+def print_data(data):
+    """Print the dataset."""
+    for agent in range(len(data)):
+        border = "*****************************************************"
+        print_colored(border, "cyan")
+        print_colored(f"***** Agent {agent+1}", "cyan")
+        print_colored(data[agent], "cyan")
+        print_colored(border, "cyan")
+
+def select_best_agent(performance_data):
+    """Select the best agent based on performance data."""
+    best_agent_index = performance_data.index(min(performance_data))
+    return best_agent_index
+
+def need_retraining(best_agent, performance_data, threshold=0.1):
+    """Check if the best agent's performance is above a certain threshold, indicating a need for retraining."""
+    return performance_data[best_agent] > threshold
+
+def evaluate_agent_performance(test_actu, test_pred):
+    """Evaluate the performance of each agent using Mean Absolute Error."""
+    performance_data = []
+    for actual, prediction in zip(test_actu, test_pred):
+        mae = mean_absolute_error(actual['Actual'], prediction['Prediction'])
+        performance_data.append(mae)
+    return performance_data
