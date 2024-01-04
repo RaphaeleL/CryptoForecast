1bdb875512f840978fb692ef8a14b7c37a2c4cf7
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2024-01-04 08:15:42 +0100

    Clean Code

--- forecast.py
+++ forecast.py
@@ -1,27 +1,33 @@
 #!/usr/bin/env python3
 
-import threading, os, warnings
+import tensorflow as tf
+import threading
+import os
+import warnings
 from sklearn.model_selection import KFold
-from utils import *
+from utils import argument_parser, load_history, predict_future, plot, \
+        load_and_preprocess_data, normalize_data, create_dataset, \
+        evaluate_agent_performance, select_best_agent, performance_output
 
 warnings.filterwarnings('ignore')
-import tensorflow as tf
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '0'
-tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)  
+tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
 
-def crypto_forecast(data, scaler, X, y, kf, agent, args, test_pred, test_actu, real_pred, prediction_days=1):
+
+def crypto_forecast(data, scaler, X, y, f, agent, args, train, test, val):
     """Forecast cryptocurrency prices."""
-    test_pred, test_actu = load_history(args, kf, agent, X, y, scaler, data, test_pred, test_actu)
-    real_pred = predict_future(args, kf, agent, X, y, scaler, data, real_pred, prediction_days)
-    return test_pred, test_actu, real_pred
+    train, test = load_history(args, f, agent, X, y, scaler, data, train, test)
+    val = predict_future(args, kf, agent, X, y, scaler, data, val)
+    return train, test, val
+
 
 def main(coin, data, scaler, X, y, kf, args):
-    threads, test_pred, test_actu, real_pred = [], [], [], []
+    threads, train, test, val = [], [], [], []
 
     for agent in range(args.agents):
         thread = threading.Thread(
             target=crypto_forecast,
-            args=(data, scaler, X, y, kf, agent, args, test_pred, test_actu, real_pred, args.prediction),
+            args=(data, scaler, X, y, kf, agent, args, train, test, val),
         )
         threads.append(thread)
         thread.start()
@@ -30,11 +36,12 @@ def main(coin, data, scaler, X, y, kf, args):
         thread.join()
 
     if args.debug > 1:
-        performance_data = evaluate_agent_performance(test_actu, test_pred)
+        performance_data = evaluate_agent_performance(test, train)
         best_agent = select_best_agent(performance_data)
-        percentage_change =  performance_output(args, real_pred, best_agent, coin)
-        mae_score = evaluate_agent_performance(test_actu, test_pred)[best_agent]
-        plot(args.coin, best_agent, real_pred, args.prediction, mae_score, percentage_change)
+        p_change = performance_output(args, val, best_agent, coin)
+        mae_score = evaluate_agent_performance(test, train)[best_agent]
+        plot(args.coin, best_agent, val, args.prediction, mae_score, p_change)
+
 
 if __name__ == "__main__":
     args = argument_parser()
--- utils.py
+++ utils.py
@@ -12,6 +12,7 @@ from keras.regularizers import l2
 from keras.optimizers.legacy import Adam
 from tqdm.keras import TqdmCallback
 
+
 def load_and_preprocess_data(ticker):
     """Load and preprocess data from the given path."""
     data = yf.download(ticker)
@@ -20,6 +21,7 @@ def load_and_preprocess_data(ticker):
     data.set_index("Date", inplace=True)
     return stretch_data(data)
 
+
 def stretch_data(data, stretch_factor=4):
     """Stretch daily data to a specified hourly interval."""
     data.index = pd.to_datetime(data.index)
@@ -27,38 +29,47 @@ def stretch_data(data, stretch_factor=4):
     stretched = data.resample(interval).interpolate(method="time")
     return stretched
 
+
 def normalize_data(data):
     """Normalize data using Min-Max Scaler."""
     scaler = MinMaxScaler(feature_range=(0, 1))
     return scaler, scaler.fit_transform(data)
 
+
 def create_dataset(dataset):
-    """Create dataset matrix for training and testing using the full dataset."""
+    """Create dataset matrix for training and testing using the full dataset"""
     dataX, dataY = [], []
     for i in range(len(dataset) - 1):
-        a = dataset[i : i + 1, 0]
+        a = dataset[i: i + 1, 0]
         dataX.append(a)
         dataY.append(dataset[i + 1, 0])
     return np.array(dataX).reshape(-1, 1, 1), np.array(dataY)
 
+
 def plot(coin, agent, val, prediction, mae_score, percentage_change):
-    """Plot predictions and actuals for the best agents in a single GUI window."""
-    val_agent = val[agent] if isinstance(val, list) and len(val) > agent else val
+    """Plot predictions for the best agent"""
+    val_agent = val[agent]
     pre_days = prediction * 12
-    pre_days = min(pre_days, len(val_agent)) 
+    pre_days = min(pre_days, len(val_agent))
     trend = "rising" if percentage_change > 0 else "falling"
-    plt.figure(figsize=(10, 5)) 
-    plt.plot(val_agent.head(pre_days).index, val_agent['Prediction'][:pre_days], label="Prediction", alpha=0.7)
-    plt.title(f"{coin} Prediction by #{agent+1} with MAE {mae_score:.2f} - It is {trend} by {percentage_change}% within {pre_days/24} days.")
+    plt.figure(figsize=(10, 5))
+    indexes = val_agent.head(pre_days).index
+    labels = val_agent['Prediction'][:pre_days]
+    plt.plot(indexes, labels, label="Prediction", alpha=0.7)
+    plt.title(f"{coin} Prediction by #{agent+1} with MAE {mae_score:.2f} -\
+            It is {trend} by {percentage_change}% within {pre_days/24} days.")
     plt.xlabel("Days")
     plt.ylabel("Price")
-    plt.gca().xaxis.set_major_locator(mdates.DayLocator(interval=1))
-    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter("%Y-%m-%d - %H:%M"))
+    formatter = mdates.DateFormatter("%Y-%m-%d - %H:%M")
+    locator = mdates.DayLocator(interval=1)
+    plt.gca().xaxis.set_major_locator(locator)
+    plt.gca().xaxis.set_major_formatter(formatter)
     plt.setp(plt.gca().get_xticklabels(), rotation=45, ha="right")
     plt.grid(True)
     plt.legend()
     plt.tight_layout()
     plt.show()
+
 
 def split(X, y, train_index, test_index=None):
     """Split the dataset into training and testing sets."""
@@ -69,6 +80,7 @@ def split(X, y, train_index, test_index=None):
         y_test = y[test_index]
         return X_train, X_test, y_train, y_test
     return X_train, y_train
+
 
 def build_and_compile_model(num_features):
     """Build and compile the Keras Sequential model."""
@@ -86,7 +98,8 @@ def build_and_compile_model(num_features):
     model.compile(optimizer=Adam(0.001), loss="mse")
     return model
 
-def train(X, X_train, y_train, batch_size, epochs, debug_level):
+
+def train_model(X, X_train, y_train, batch_size, epochs, debug_level):
     """Train the model."""
     tqdm_verbose = 1 if debug_level > 1 else 0
     callbacks = [TqdmCallback(verbose=tqdm_verbose)] if debug_level > 0 else []
@@ -101,9 +114,11 @@ def train(X, X_train, y_train, batch_size, epochs, debug_level):
     )
     return model
 
+
 def argument_parser():
     """Parse command line arguments."""
-    argparser = argparse.ArgumentParser(description="Cryptocurrency Price Prediction")
+    argparser = argparse.ArgumentParser(
+        description="Cryptocurrency Price Prediction")
     argparser.add_argument("--coin", type=str, default="LTC-EUR")
     argparser.add_argument("--batch_size", type=int, default=32)
     argparser.add_argument("--epochs", type=int, default=100)
@@ -114,13 +129,15 @@ def argument_parser():
     args = argparser.parse_args()
     return args
 
+
 def check(first_pred, last_actual, p=0.05):
-    """Check if the prediction is within the given percentage of the actual value."""
+    """Check if the prediction is valid."""
     if first_pred >= last_actual * (1-p) and first_pred <= last_actual * (1+p):
         return True
     return False
 
-def print_colored(to_print, color, end="\n"):
+
+def cprint(to_print, color, end="\n"):
     """Print text in the given color."""
     colors = {
         "red": "\033[91m",
@@ -133,64 +150,79 @@ def print_colored(to_print, color, end="\n"):
     }
     print(f"{colors.get(color, '')}{to_print}{colors['end']}", end=end)
 
+
 def select_best_agent(performance_data):
     """Select the best agent based on performance data."""
     best_agent_index = performance_data.index(min(performance_data))
     return best_agent_index
 
+
 def need_retraining(best_agent, performance_data, threshold=0.1):
-    """Check if the best agent"s performance is above a certain threshold, indicating a need for retraining."""
+    """Check if the best agent's performance need a retraining"""
     return performance_data[best_agent] > threshold
 
-def evaluate_agent_performance(test_actu, test_pred):
+
+def evaluate_agent_performance(test, train):
     """Evaluate the performance of each agent using Mean Absolute Error."""
     performance_data = []
-    for actual, prediction in zip(test_actu, test_pred):
+    for actual, prediction in zip(test, train):
         mae = mean_absolute_error(actual["Actual"], prediction["Prediction"])
         performance_data.append(mae)
     return performance_data
 
-def load_history(args, kf, agent, X, y, scaler, data, test_pred, test_actu):
+
+def load_history(args, kf, agent, X, y, scaler, data, train, test):
     """Load history for each agent."""
     predictions = []
     actuals = []
-    print_colored(f"Load History for Agent {agent+1:02d}/{args.agents:02d}", "yellow")
+    cprint(
+        f"Load History for Agent {agent+1:02d}/{args.agents:02d}", "yellow")
     for train_index, test_index in kf.split(X):
         X_train, X_test, y_train, y_test = split(X, y, train_index, test_index)
-        model = train(X, X_train, y_train, args.batch_size, args.epochs, args.debug)
+        btsz, epc, dbg = args.batch_size, args.epochs, args.debug
+        model = train_model(X, X_train, y_train, btsz, epc, dbg)
         prediction = scaler.inverse_transform(model.predict(X_test, verbose=0))
         predictions.extend(prediction)
         actuals.extend(scaler.inverse_transform(y_test.reshape(-1, 1)))
 
     test_dates = data.index[test_index].to_pydatetime()
-    test_pred.append(pd.DataFrame(prediction, index=test_dates, columns=['Prediction']))
-    test_actu.append(pd.DataFrame(scaler.inverse_transform(y_test.reshape(-1, 1)), index=test_dates, columns=['Actual']))
+    train.append(pd.DataFrame(
+        prediction, index=test_dates, columns=['Prediction']))
+    test.append(pd.DataFrame(scaler.inverse_transform(
+        y_test.reshape(-1, 1)), index=test_dates, columns=['Actual']))
 
-    return test_pred, test_actu
+    return train, test
 
-def predict_future(args, kf, agent, X, y, scaler, data, real_pred, prediction_days):
+
+def predict_future(args, kf, agent, X, y, scaler, data, val):
     """Predict future cryptocurrency prices."""
-    real_predictions = []
-    print_colored(f"Predict Future for Agent {agent+1:02d}/{args.agents:02d}", "purple")
+    valictions = []
+    agent_str = f"{agent+1:02d}/{args.agents:02d}"
+    cprint(f"Predict Future for Agent {agent_str}", "purple")
     for train_index, _ in kf.split(X):
-        X_train, y_train = split(X, y, train_index) 
-        model = train(X, X_train, y_train, args.batch_size, args.epochs, args.debug)
-        prediction = scaler.inverse_transform(model.predict(X[-(prediction_days*12):], verbose=0))
-        real_predictions.extend(prediction)
+        X_train, y_train = split(X, y, train_index)
+        btsz, epc, dbg = args.batch_size, args.epochs, args.debug
+        model = train_model(X, X_train, y_train, btsz, epc, dbg)
+        prediction = scaler.inverse_transform(model.predict(X, verbose=0))
+        valictions.extend(prediction)
 
     last_day = data.index[-1]
     next_day = last_day + pd.Timedelta(hours=1)
-    future_dates = pd.date_range(start=next_day, periods=len(real_predictions), freq='H')
-    real_pred.append(pd.DataFrame(real_predictions, index=future_dates, columns=['Prediction']))
+    future_dates = pd.date_range(
+        start=next_day, periods=len(valictions), freq='H')
+    df = pd.DataFrame(valictions, index=future_dates, columns=['Prediction'])
+    val.append(df)
 
-    return real_pred
+    return val
 
-def performance_output(args, real_pred, best_agent, coin):
+
+def performance_output(args, val, best_agent, coin):
     """Output the performance of the best agent."""
     duration = args.prediction * 12
-    first_entry = real_pred[best_agent].tail(duration).iloc[0]
-    last_entry = real_pred[best_agent].tail(duration).iloc[-1]
+    first_entry = val[best_agent].tail(duration).iloc[0]
+    last_entry = val[best_agent].tail(duration).iloc[-1]
     first_entry_value = first_entry['Prediction']
     last_entry_value = last_entry['Prediction']
-    percentage_change = round(((last_entry_value - first_entry_value) / first_entry_value) * 100, 2)
+    percentage_change = round(
+        ((last_entry_value - first_entry_value) / first_entry_value) * 100, 2)
     return percentage_change
