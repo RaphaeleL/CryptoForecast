863fea08532bf64beeb95ece1b7020924e7af46b
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2024-01-06 07:25:01 +0100

    Declutter

--- forecast.py
+++ forecast.py
@@ -1,26 +1,19 @@
 #!/usr/bin/env python3
 
-from utils import load_history, predict_future, argument_parser, get_data, \
-    get_weight_file_path, cprint, plot
-
-
-def crypto_forecast(data, scaler, X, y, kf, args):
-    trn, tst, prd, dur = [], [], [], args.prediction * 24
-    model, trn, tst = load_history(args, kf, X, y, scaler, data, trn, tst)
-    prd = predict_future(args, kf, X, y, scaler, data, prd, dur)
-    return model, trn, tst, prd
+from utils import load_history, predict_future, cprint, \
+    preprocess, postprocess, get_weight_file_path
 
 
 if __name__ == "__main__":
-    args = argument_parser()
-    data, scaler, X, y, kf = get_data(args)
-    forecast_result = crypto_forecast(data, scaler, X, y, kf, args)
-    model, trn, tst, prd = forecast_result
+    args, data, scaler, X, y, = preprocess()
 
     if args.retrain:
+        model, trn, tst = load_history(args, X, y, scaler, data)
         model_filename = get_weight_file_path(args.coin)
         model.save_weights(model_filename)
         cprint(f"Saved model weights to '{model_filename}'", "green")
-
-    if args.debug > 1:
-        plot(args.coin, prd, args.prediction)
+    else:
+        dur = args.prediction * 24
+        model, trn, tst = load_history(args, X, y, scaler, data)
+        prd = predict_future(args, X, y, scaler, data, dur)
+        postprocess(args, prd)
