46c53cdaca7ea04b7a7974f9116e7a9932a84f86
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2024-01-08 13:52:14 +0100

    Add TODO's

--- main.py
+++ main.py
@@ -6,6 +6,16 @@ from utils import plot
 
 if __name__ == "__main__":
 
+    # TODO:
+    #   - Add an '--agents' CLI argument.
+    #       - This will allow the user to use Agents instead of Weight Files
+    #       - Number of Agents will be automatically determined by the number of cores
+    #       - The Forecast Class will automalically determine if Agents or Weight Files are used
+    #   - Add a 'show()' Method to Visualize the Prediction and the Validation if Agents are used
+    #       - For Step 5
+    #   - Add a 'validate()' Method to Validate the Prediction
+    #       - For Step 3
+
     cf = CryptoForecast()
 
     # Step 0 - (Re-)Train the Neural Network
@@ -17,7 +27,7 @@ if __name__ == "__main__":
     # Step 2 - Predict the Future Price of the Ticker
     cf.predict_future()
 
-    # TODO: Step 3 - Validate the Prediction
+    # Step 3 - Validate the Prediction
 
     # Step 5: Visualize the Prediction
     plot(cf.prediction_days, cf.forecast_data, cf.ticker)
