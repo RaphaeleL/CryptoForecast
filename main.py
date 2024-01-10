f15a7321f7d61f0d0fd205ecbc0647f607308fe1
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2024-01-10 07:07:23 +0100

    Remove TODOs

--- main.py
+++ main.py
@@ -5,20 +5,6 @@ from forecast import CryptoForecast
 
 def main(cf):
 
-    # TODO
-    #   - NEWS
-    #       - Ein weiteres Feature signalisiert den Trend, wodurch
-    #         News (also der Trend) beeinflusst werden kann.
-    #       - Minütliche Daten der letzten 7 Tage sind mit Yahoo möglich.
-    #         Damit kann eine News zwar nicht eingearbeitet werden. Jedoch,
-    #         kann rechtzeitig die Folgen erkannt werden um entsprechend,
-    #         mit der Vorhersage darauf einzugehen. Eine Vorhersage MUSS
-    #         weniger als 60 Sekunden dauern.
-    #   - VALIDATION
-    #       - Trend Analysis (Up, Down, Sideways, etc.)
-    #       - LLMs (ChatGPT, LLaMa, etc.))
-    #       - For Step 3
-
     if cf.should_retrain:
         cf.load_history()
         cf.stop_time("to retrain the model")
