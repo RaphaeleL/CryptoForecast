2fd6d8ef4035fd54043b3934ba2ab7acc04f2001
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2024-01-14 14:38:13 +0100

    Monte Carlo Simulation

--- validation.py
+++ validation.py
@@ -1,37 +1,68 @@
 from utils import get_colored_text
 
+import numpy as np
+import pandas as pd
+import yfinance as yf
+from matplotlib import pyplot as plt
+
 keys = ["Monte Carlo Simulation", "Trend Analysis", "Asking a LLM"]
 space = lambda x, y: "." * (len(max(keys, key=len)) - len(x) + 5 + y)
 
 
 def convert_result_to_text(text):
-    if text is True:
+    if text:
         return get_colored_text("passed", "green")
-    elif text is False:
+    elif not text:
         return get_colored_text("failed", "red")
     else:
-        return ""
+        return get_colored_text("n/a", "yellow")
 
 
-def monte_carlo_simulation(cf):
-    return True
+def monte_carlo_simulation(cf, num_simulations=1000, num_days=30, confidence_interval=0.95, plot=False):
+    last_price = cf.data['Close'][-1]
+    simulation_df = pd.DataFrame()
+    for x in range(num_simulations):
+        count = 0
+        daily_volatility = cf.data['Close'].pct_change().std()
+        price_series = []
+        price = last_price * (1 + np.random.normal(0, daily_volatility))
+        price_series.append(price)
+        for y in range(num_days):
+            if count == 251:
+                break
+            price = price_series[count] * (1 + np.random.normal(0, daily_volatility))
+            price_series.append(price)
+            count += 1
+        simulation_df[x] = price_series
+
+    lower_bound = simulation_df.quantile((1 - confidence_interval) / 2)
+    upper_bound = simulation_df.quantile(1 - (1 - confidence_interval) / 2)
+    actual_price = yf.Ticker(cf.ticker).history(period="1d")["Close"][0]
+
+    if plot:
+        plt.title(f"{cf.ticker} Monte Carlo Simulation")
+        plt.plot(simulation_df)
+        plt.show()
+
+    return (lower_bound <= actual_price).any() and (actual_price <= upper_bound).any()
 
 
 def trend_analysis(cf):
-    return True
+    # TODO: Swing Trading also Stündliche Analyse
+    return False
 
 
 def llm(cf):
-    return True
+    # TODO: OpenAI ChatGPT API
+    return False
 
 
 def validate(cf):
-    mcs_res = monte_carlo_simulation(cf)
-    ta_res = trend_analysis(cf)
-    llm_res = llm(cf)
-
-
-    results = {"Monte Carlo Simulation": mcs_res, "Trend Analysis": ta_res, "Asking a LLM": llm_res}
+    results = {
+            "Monte Carlo Simulation": monte_carlo_simulation(cf),
+            "Trend Analysis": trend_analysis(cf),
+            "Asking a LLM": llm(cf)
+    }
 
     for index, key in enumerate(keys):
         delim = "└──" if index == len(keys) - 1 else "├──"
