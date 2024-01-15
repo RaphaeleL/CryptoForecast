a2393d00f1c07c92144a5c54e7ddd09b7754c17e
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2024-01-15 14:26:50 +0100

    Rremove TODO

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ def main(cf=CryptoForecast()):
         return
 
     if cf.args.auto:
-        for ticker in get_full_ticker_list(): # TODO: Add threading
+        for ticker in get_full_ticker_list():
             tcf = CryptoForecast(ticker)
             tcf.load_history()
             tcf.predict_future()
